2227ed6c6ca42f1fcd858687ea1f13a0897d8575
JiashuGuo <jiashu1002@gmail.com>
2018-04-03 20:47:38 -0700

    add one more list

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -6,30 +6,30 @@ import { DataService } from '../data.service';
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
-  animations: [
+  // animations: [
 
-    trigger('goals', [
-      transition('* => *', [
+  //   trigger('goals', [
+  //     transition('* => *', [
 
-        query(':enter', style({ opacity: 0 }), {optional: true}),
+  //       query(':enter', style({ opacity: 0 }), {optional: true}),
 
-        query(':enter', stagger('300ms', [
-          animate('.6s ease-in', keyframes([
-            style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
-            style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
-          ]))]), {optional: true})
-          ,
-        query(':leave', stagger('300ms', [
-          animate('.6s ease-out', keyframes([
-            style({opacity: 1, transform: 'translateY(0)', offset: 0}),
-            style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
-            style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
-          ]))]), {optional: true})
-      ])
-    ])
+  //       query(':enter', stagger('300ms', [
+  //         animate('.6s ease-in', keyframes([
+  //           style({opacity: 0, transform: 'translateY(-75%)', offset: 0}),
+  //           style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+  //           style({opacity: 1, transform: 'translateY(0)',     offset: 1.0}),
+  //         ]))]), {optional: true})
+  //         ,
+  //       query(':leave', stagger('300ms', [
+  //         animate('.6s ease-out', keyframes([
+  //           style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+  //           style({opacity: .5, transform: 'translateY(35px)',  offset: 0.3}),
+  //           style({opacity: 0, transform: 'translateY(-75%)',     offset: 1.0}),
+  //         ]))]), {optional: true})
+  //     ])
+  //   ])
 
-  ]
+  // ]
 
 })
 export class HomeComponent implements OnInit {
@@ -38,7 +38,7 @@ export class HomeComponent implements OnInit {
   btnText:string='Add an item';
   goalText: string='My life goal';
   goals = [];
-
+  dones = [];
   constructor(private _data:DataService) { }
    
   ngOnInit() {
@@ -55,9 +55,11 @@ addItem(){
 	this._data.changeGoal(this.goals);
 }
 removeItem(i) {
+  this.dones.push(this.goals[i]);
   this.goals.splice(i,1);
   this.itemCount = this.goals.length;
   this._data.changeGoal(this.goals);
+
 }
 
 }
